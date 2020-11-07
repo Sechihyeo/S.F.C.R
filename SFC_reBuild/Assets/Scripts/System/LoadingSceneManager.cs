a62b6dcd6dc529a533bc3bfecb9efbcc93c9a3ee
Dead-DogBird <48205134+Dead-DogBird@users.noreply.github.com>
2020-11-08 01:42:33 +0900

    20201108

---
+++ SFC_reBuild/Assets/Scripts/System/LoadingSceneManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using static System.Math;
+public class LoadingSceneManager : MonoBehaviour
+{
+    public static string nextScene;
+
+    [SerializeField]
+    Image progressBar;
+
+
+    public Text percent;
+
+    private void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    string nextSceneName;
+    public static void LoadScene(string sceneName)
+    {
+        nextScene = sceneName;
+        SceneManager.LoadScene("Loading");
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+
+
+        float timer = 0.0f;
+        while (!op.isDone)
+        {
+            yield return null;
+
+            timer += Time.deltaTime;
+
+            if (op.progress >= 0.9f)
+            {
+                    op.allowSceneActivation = true;
+            }
+            else
+            {
+                progressBar.fillAmount = op.progress/0.9f;
+            }
+            percent.text= (progressBar.fillAmount*100).ToString("F1")+" %";
+        }
+    }
+}
---
+++ SFC_reBuild/Assets/Scripts/UIscript/Intro_Scence.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Intro_Scence : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Main_menu");
+        }
+    }
+}
