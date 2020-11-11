4cf19e9fda92342b716faf727f0bd3cd17a20bb5
Dead-DogBird <48205134+Dead-DogBird@users.noreply.github.com>
2020-11-12 03:43:46 +0900

    20201112

--- SFC_reBuild/Assets/Scripts/System/MainMenu_Manager.cs
+++ SFC_reBuild/Assets/Scripts/System/MainMenu_Manager.cs
@@ -7,10 +7,12 @@ public class MainMenu_Manager : MonoBehaviour
     AudioSource buttonSoundSource;
     [SerializeField]
     AudioClip pressSfx;
+      [SerializeField]
+    AudioClip pressSfx2;
     [SerializeField]
     float sfxvolume=1;
     [SerializeField]
-    Image fadeImg;
+    Image fadeImg,fadelogo;
     bool isFadeIn=true;
     float fadeImgAlpha=0;
     // Start is called before the first frame update
@@ -19,27 +21,71 @@ public class MainMenu_Manager : MonoBehaviour
         buttonSoundSource = gameObject.AddComponent<AudioSource>();
         buttonSoundSource.clip=pressSfx;
         buttonSoundSource.volume=sfxvolume;
+        fadeImg.color=new Color(1,1,1,1);
         fadeImgAlpha=fadeImg.color.a;
-        //fadeImg.color=new Color(1,1,1,0);
+        fadelogo.color=new Color(1,1,1,0);
+        StartCoroutine(FadeIn());
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(isFadeIn)
-        fadeImgAlpha+=(0-fadeImgAlpha)/10;
-        else
-        fadeImgAlpha+=(1-fadeImgAlpha)/10;
+        // if(isFadeIn)
+        // fadeImgAlpha+=(0-fadeImgAlpha)/10;
+        // else
+        // //fadeImgAlpha+=(1-fadeImgAlpha)/10;
 
-        fadeImg.color=new Color(1,1,1,fadeImgAlpha);
+        // fadeImg.color=new Color(1,1,1,fadeImgAlpha);
+        
     }
     
     void press_buttonSfx()
     {
+        buttonSoundSource.clip=pressSfx;
+        buttonSoundSource.Play();
+    }
+    void press_buttonSfx2()
+    {
+        buttonSoundSource.clip=pressSfx2;
         buttonSoundSource.Play();
     }
     public void TestButton()
     {
         press_buttonSfx();
+        StartCoroutine(FadeOut("Ingame"));
+        //LoadingSceneManager.LoadScene("Ingame");
+    }
+    public void SetButton()
+    {
+        press_buttonSfx2();
+ 
+    }
+    IEnumerator FadeIn()
+    {      
+        float fade = 1;
+        while (fade >= 0)
+        {
+            fade-=0.01f;
+            fadeImg.color = new Color(1, 1, 1, fade);
+           // Debug.Log("페이드 아웃중"+fade);
+            yield return null;
+        }
+        Debug.Log(fadeImg.color.a);
+    }
+    IEnumerator FadeOut(string sceneName)
+    {
+        yield return new WaitForSeconds(1f);
+        float fade = 0;
+        while (fade <= 1)
+        {
+            fade+=0.01f;
+            fadeImg.color = new Color(0, 0, 0, fade);
+            fadelogo.color=new Color(1,1,1,fade*0.3f);
+           // Debug.Log("페이드 아웃중"+fade);
+            yield return null;
+
+        }
+        yield return new WaitForSeconds(1f);
+        LoadingSceneManager.LoadScene(sceneName);
     }
 }
---
+++ SFC_reBuild/Assets/Scripts/UIscript/Mainfade_logo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Mainfade_logo : MonoBehaviour
+{
+    Image parImg;
+    Image myImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parImg=GetComponentInParent<Image>();
+        myImg=GetComponent<Image>();
+        myImg.color=new Color(1,1,1,parImg.color.a);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myImg.color=new Color(1,1,1,parImg.color.a);
+    }
+}
