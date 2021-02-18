d7b4850e76d3f174421dfe605a0a774f3816f65f
noeulnight <noeulnight.mail@gmail.com>
2021-02-18 12:38:02 +0900

    asdf

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const db = knex({ client: 'mysql', connection: { host: 'localhost', user: 'sfc',
 app.get('/kdr/info', async (req, res) => {
   const { id } = req.query
   if (!id) return res.send('ID Undefind').status(404)
+  if (!Number(id)) return res.send('Wrong Type').status(404)
   const [exist] = await db.where({ id }).from('user').select('*')
   if (!exist) return res.send('Can not get user').status(404)
   const kdr = exist.k / exist.d
@@ -16,6 +17,7 @@ app.get('/kdr/info', async (req, res) => {
 app.get('/kdr/push', async (req, res) => {
   const { id, kill, death } = req.query
   if (!id || !kill || !death) return res.send('Undefind Query').status(404)
+  if (!Number(id) || !Number(kill) || !Number(death)) return res.send('Wrong Type').status(404)
   const [exist] = await db.where({ id }).from('user').select('*')
   if (!exist) await db.insert({ id, k: kill, d: death }).from('user').select('*')
   else await db.update({ k: exist.k + kill, d: exist.d + death }).from('user').select('*').where({ id })
